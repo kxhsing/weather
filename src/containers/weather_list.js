b9e36d0c7049fda98e39adfe770282613bc10bb8
Karen Hsing <kxhsing@gmail.com>
2017-07-29 19:59:17 -0700

    Sparklines charts working, adding Google Maps

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,20 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import Chart from '../components/chart';
 
 
 class WeatherList extends Component {
+    renderWeather(cityData) {  
+
+        const name = cityData.city.name;
+        const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp * (9/5.0) - 459.67);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
+
+        return (
+            <tr key={name}>
+                <td>{name}</td>
+                <td><Chart data={temps} color="orange" units="F" /></td>
+                <td><Chart data={pressures} color="green" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
+            </tr>
+        );
+
+    }
+
     render () {
         return (
             <table className="table table-hover">
                 <thead>
                     <tr>
                         <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>Temperature (F)</th>
+                        <th>Pressure (hPa)</th>
+                        <th>Humidity (%)</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {this.props.weather.map(this.renderWeather)}
                 </tbody>
             </table>
         );
